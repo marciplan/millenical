a283ccd76c910c6f6bce6da29b8eef440fdba3a1
marciplan <ik@marcianoplanque.nl>
2020-10-24 00:56:24 +0200

    Added GA

--- pages/index.js
+++ pages/index.js
@@ -1,42 +1,45 @@
 import Head from "next/head";
+import Layout from "../components/layout";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>A new kind of calendar. Again.</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    <Layout>
+      <div className={styles.container}>
+        <Head>
+          <title>A new kind of calendar. Again.</title>
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>Millenical</h1>
+        <main className={styles.main}>
+          <h1 className={styles.title}>Millenical</h1>
 
-        <p className={styles.description}>
-          <code className={styles.code}>
-            The calendar for those with a full agenda
-          </code>
-        </p>
-      </main>
-      <content className={styles.secondary}>
-        <p className={styles.holder}>
-          Busy lifes we're living, huh? We want to be productive at work and
-          make the most of the time with our friends and families. It's time our
-          calendar took that into account.
-        </p>
-        <p className={styles.code}>Launching in 2021</p>
-      </content>
-      <footer className={styles.footer}>
-        <a
-          href="mailto:hey@millenical.com"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Let's keep in touch:&nbsp;
-          <span className={styles.highlight}>hey@millenical.com</span>
-        </a>
-      </footer>
-    </div>
+          <p className={styles.description}>
+            <code className={styles.code}>
+              The calendar for those with a full agenda
+            </code>
+          </p>
+        </main>
+        <content className={styles.secondary}>
+          <p className={styles.holder}>
+            Busy lifes we're living, huh? We want to be productive at work and
+            make the most of the time with our friends and families. It's time
+            our calendar took that into account.
+          </p>
+          <p className={styles.code}>Launching in 2021</p>
+        </content>
+        <footer className={styles.footer}>
+          <a
+            href="mailto:hey@millenical.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Let's keep in touch:&nbsp;
+            <span className={styles.highlight}>hey@millenical.com</span>
+          </a>
+        </footer>
+      </div>
+    </Layout>
   );
 }
 
